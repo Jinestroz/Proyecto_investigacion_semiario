a07193dce6a379d8acb5a4c8fc18932df9c128dc
Jose Alfonzo Inestroza Rodriguez <jinestroza87@hotmail.com>
2021-07-02 10:18:20 -0600

    Analisis Prueba T y Prueba F
    Análisis Regresión Espuria Prueba-t y Prueba-F

--- Base de datos Python/seminario_python.py
+++ Base de datos Python/seminario_python.py
@@ -1,9 +1,3 @@
-import xlrd
-book = xlrd.open_workbook("base_datos_empleo.xlsx")
-print("The number of worksheets is {0}".format(book.nsheets))
-print("Worksheet name(s): {0}".format(book.sheet_names()))
-sh = book.sheet_by_index(0)
-print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
-print("Cell D30 is {0}".format(sh.cell_value(rowx=29, colx=3)))
-for rx in range(sh.nrows):
-    print(sh.row(rx))
+import pandas as pd
+df = pd.read_excel(io = "base_datos_empleo.xlsx", sheet_name="BASE DE DATOS 2001 Y 2013 MUNIC")
+print(df)
